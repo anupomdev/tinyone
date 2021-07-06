f80fe00b703eea9ea9fc3ce68454ebff7b14e407
anupomdev <anupomchakraborty13@gmail.com>
2021-07-06 12:40:55 +0600

    tinyone_porosh

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    'use strict';
+
+    // banner slider
+    $('#banner_part').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+      });
+
+
+
+});
